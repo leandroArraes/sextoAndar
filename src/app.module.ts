8783cd93dd99252520cd09988af0b0c96afe814e
leandroArraes <leandro.arraes.182@gmail.com>
2024-06-26 00:54:22 +0000

    atualizando agora

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImovelModule } from './imovel/imovel.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ImovelModule, UsuarioModule],
+  imports: [
+    ImovelModule,
+    UsuarioModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
